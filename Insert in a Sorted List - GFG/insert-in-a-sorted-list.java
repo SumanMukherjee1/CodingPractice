318e725df0aa77a7ec622b9d7610b38dc0f0b22d
Suman Mukherjee <136717302+SumanMukherjee1@users.noreply.github.com>
2023-10-08 11:19:03 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head1, int key) {
+        // Add your code here.
+        if(key<head1.data){
+            Node p=new Node(key);
+            p.next=head1;
+            head1=p;
+        }
+        else
+        {
+            Node ptr=head1,prev=null;
+            while(ptr!=null && key>=ptr.data ){
+            prev=ptr;
+            ptr=ptr.next;}
+            Node p=new Node(key);
+            if(ptr==null){
+             prev.next=p;
+             p.next=null;
+            }
+            else{
+                prev.next=p;
+                p.next=ptr;
+            }
+        }
+        return head1;
+    }
+}
